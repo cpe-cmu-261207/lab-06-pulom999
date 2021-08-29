98bab3faaa07502e200ebe0861ef3e824f6f057e
pulom999 <76034695+pulom999@users.noreply.github.com>
2021-08-29 14:28:03 +0700

    update

---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div className='text-center space-y-3'>
+            <p className='text-2xl font-semibold'>About me</p>
+            <p className='text-xl'>Kanaphat Prapet 630612095</p>
+        </div>
+    )
+}
+
+export default About
